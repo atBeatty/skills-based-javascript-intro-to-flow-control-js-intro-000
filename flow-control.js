8c58d741adc3f3b59f7b0886ef4993a418935b26
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-13 23:56:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -33,7 +33,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case age <= 19 && age >= 13:
+    case age < 19 && age >= 13:
       return "You are a teenager";
       break;
     default:
