6da55964202ac9c3b155922908decd6607170e56
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-13 23:51:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,9 +27,18 @@ function ageChecker(age) {
 // Define a function ternaryTeenager that accepts age as a parameter. The body of the function should use the ternary operator to return
 // "You are a teenager" if age is between 13-19 and returns "You are not a teenager" if the age is anything else.
 function ternaryTeenager(age) {
-  return (age <= 19 && age >= 13) ? "You are a teenager" : "You are not a teenager"
+  return (age <= 19 && age >= 13) ? "You are a teenager" : "You are not a teenager";
 
 }
 
 function switchAge(age) {
+  switch(age) {
+    case 13, 14, 15, 16, 17, 18, 19:
+      return "You are a teenager";
+      break;
+    default:
+      return "You have an age";
+      break;
+  }
+
 }
