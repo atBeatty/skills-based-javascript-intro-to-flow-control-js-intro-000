0b1b0c7fea53c5f906233521444dd61711fcca00
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-13 23:40:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,5 +32,14 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  switch(age) {
+    case 13, 14, 15, 16, 17, 18, 19:
+      return "You are a teenager";
+      break;
+    default:
+      return "You have an age"
+    
+    default: 
+  }
 
 }
